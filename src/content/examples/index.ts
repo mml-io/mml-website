ba5d96636da33c0f490d6d1620d593890ae5fb03
Ben Miller <benito.m@gmail.com>
2023-06-14 17:32:04 +0100

    A clock

--- src/content/examples/index.ts
+++ src/content/examples/index.ts
@@ -1,4 +1,5 @@
 import { basicExample } from "@/src/content/examples/basic";
+import { clockExample } from "@/src/content/examples/clock";
 import { lotsOfCubesExample } from "@/src/content/examples/lots-of-cubes";
 import { lotsOfSpheresExample } from "@/src/content/examples/lots-of-spheres";
 
@@ -6,4 +7,5 @@ export const examples = {
   [basicExample.name]: basicExample,
   [lotsOfCubesExample.name]: lotsOfCubesExample,
   [lotsOfSpheresExample.name]: lotsOfSpheresExample,
+  [clockExample.name]: clockExample,
 };
