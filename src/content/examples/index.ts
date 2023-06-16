7f0145e1ad5d544dfb06819114301ea413862d42
Ben Miller <benito.m@gmail.com>
2023-06-16 11:12:25 +0100

    Game of Life example

--- src/content/examples/index.ts
+++ src/content/examples/index.ts
@@ -1,5 +1,6 @@
 import { basicExample } from "@/src/content/examples/basic";
 import { clockExample } from "@/src/content/examples/clock";
+import { gameOfLifeExample } from "@/src/content/examples/game-of-life";
 import { lotsOfCubesExample } from "@/src/content/examples/lots-of-cubes";
 import { lotsOfSpheresExample } from "@/src/content/examples/lots-of-spheres";
 
@@ -8,4 +9,5 @@ export const examples = {
   [lotsOfCubesExample.name]: lotsOfCubesExample,
   [lotsOfSpheresExample.name]: lotsOfSpheresExample,
   [clockExample.name]: clockExample,
+  [gameOfLifeExample.name]: gameOfLifeExample,
 };
