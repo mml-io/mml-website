74e12c8a177e4fd2be61b67cba706e981bbafad9
Marcus Longmuir <marcus@mpl.dev>
2023-08-07 21:42:10 +0100

    Post release blog post content

---
+++ src/content/blogPosts/aug-2023/index.ts
@@ -0,0 +1,13 @@
+import { BlogPost } from "@/types/blog-post";
+
+import body from "./body.mdx";
+
+const introducingMml: BlogPost = {
+  title: "August 2023: React, Collision Events, and More",
+  description:
+    "Check out what's been added since MML's initial release up to August 2023.",
+  body,
+  image: "/images/blog/introducing-mml.jpeg",
+};
+
+export default introducingMml;
--- src/content/blogPosts/index.ts
+++ src/content/blogPosts/index.ts
@@ -1,8 +1,10 @@
 import { BlogPostsByName } from "@/types/blog-post";
 
+import aug2023 from "./aug-2023";
 import introducingMml from "./introducing-mml";
 
 export const posts: BlogPostsByName = {
+  "aug-2023": aug2023,
   "introducing-mml": introducingMml,
 
   // Leaving this here to show how multiple posts appear on the page
