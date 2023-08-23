01be2f4bdc8be1a8fc142aab8351391e85a8a9d2
Sacha Morgese <sachamorgese@gmail.com>
2023-08-23 10:42:23 +0100

    Add collision events guide

---
+++ src/content/guides/mml-collide-events-guide/index.ts
@@ -0,0 +1,18 @@
+import { DocsGuide } from "@/types/docs-guides";
+
+import body from "./body.mdx";
+
+const collisionEvents: DocsGuide = {
+  title: "Getting Started: Collision Events in MML",
+  description: "Learn how to leverage the power of the collisions.",
+  body,
+  linkList: [
+    "Related events",
+    "Related attributes",
+    "Basic example of element collision",
+    "Example with multiple users and disconnection events",
+    "Advanced Example with collisionmove events",
+  ],
+};
+
+export default collisionEvents;
