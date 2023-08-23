4877df2e6cd5c89ad947e7a99b5b701a473cd94a
Sacha Morgese <sachamorgese@gmail.com>
2023-08-23 14:45:43 +0100

    Added react guide

---
+++ src/content/guides/mml-with-react-guide/index.ts
@@ -0,0 +1,13 @@
+import { DocsGuide } from "@/types/docs-guides";
+
+import body from "./body.mdx";
+
+const collisionEvents: DocsGuide = {
+  title: "Getting Started: MML with React",
+  // Give me a better description
+  description: "Composability, reusability, and extensibility made easy.",
+  body,
+  linkList: [],
+};
+
+export default collisionEvents;
