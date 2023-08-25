4a8e9cbc1ee174ee29d7d918a4c61ca1d59d8d05
Sacha Morgese <sachamorgese@gmail.com>
2023-08-25 20:39:07 +0100

    Added guide up to collision events

--- src/content/guides/index.ts
+++ src/content/guides/index.ts
@@ -1,4 +1,5 @@
 import collisionEvents from "@/src/content/guides/mml-collide-events-guide";
+import mmlWithReactGuide from "@/src/content/guides/mml-with-react-guide";
 import { DocsGuidesByName } from "@/types/docs-guides";
 
 import hosting from "src/content/guides/mml-starter-project-hosting";
@@ -6,4 +7,5 @@ import hosting from "src/content/guides/mml-starter-project-hosting";
 export const guides: DocsGuidesByName = {
   "mml-starter-project-hosting": hosting,
   "mml-collide-events-guide": collisionEvents,
+  "mml-with-react": mmlWithReactGuide,
 };
