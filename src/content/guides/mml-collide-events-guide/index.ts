332b82a079871656df4180379d2bafd77c09368f
Sacha Morgese <sachamorgese@gmail.com>
2023-08-25 12:19:02 +0100

    Updated content based on PR review feedback

--- src/content/guides/mml-collide-events-guide/index.ts
+++ src/content/guides/mml-collide-events-guide/index.ts
@@ -3,15 +3,15 @@ import { DocsGuide } from "@/types/docs-guides";
 import body from "./body.mdx";
 
 const collisionEvents: DocsGuide = {
-  title: "Getting Started: Collision Events in MML",
-  description: "Learn how to leverage the power of the collisions.",
+  title: "Collision Events",
+  description: "Learn how to make MML content that can react to users colliding with it.",
   body,
   linkList: [
     "Related events",
     "Related attributes",
     "Basic example of element collision",
     "Example with multiple users and disconnection events",
-    "Advanced Example with collisionmove events",
+    "Advanced example with collisionmove events",
   ],
 };
 
