ac4693301164d280ead5e395a4817a216ceec040
Sacha Morgese <sachamorgese@gmail.com>
2023-11-21 13:14:41 +0000

    Fixed descriptions

--- src/content/docs/mmlPositionLeaveEvent/index.ts
+++ src/content/docs/mmlPositionLeaveEvent/index.ts
@@ -5,14 +5,14 @@ import { CLIENT_TYPES, DocsExamples } from "@/types/docs-reference";
 export const examples: DocsExamples = {
   primary: {
     title: "Collision end event",
-    description: "The label changes color when a user enters the probe area.",
+    description: "The label changes color when a user leaves the probe area.",
     code: primary,
     baseSceneOn: false,
     clients: [CLIENT_TYPES.AVATAR],
   },
   multipleUsers: {
     title: "Multiple users",
-    description: "The label shows the connection id of the users who entered the probe area.",
+    description: "The label shows the connection id of the users who left the probe area.",
     code: multipleUsers,
     baseSceneOn: false,
     clients: [CLIENT_TYPES.AVATAR, CLIENT_TYPES.AVATAR],
