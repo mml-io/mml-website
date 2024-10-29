7a7f5166a1f293d25689fbb2e7951eb1f07eb84a
TheCodeTherapy <me@mgz.me>
2024-10-29 16:01:05 +0000

    adds a streaming to the metaverse blogpost

---
+++ src/content/blogPosts/live-stream-to-the-metaverse/index.ts
@@ -0,0 +1,13 @@
+import { BlogPost } from "@/types/blog-post";
+
+import body from "./body.mdx";
+
+const introducingMml: BlogPost = {
+  title: "Live Streaming to the Metaverse",
+  description: "An easy way to have your live event in the Metaverse.",
+  date: "2024-10-29",
+  body,
+  image: "/images/blog/live-stream-to-the-metaverse.jpg",
+};
+
+export default introducingMml;
