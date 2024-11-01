2ef1f5a6b12d34e1deb977c410de47bd77f767fd
Marcus Longmuir <marcus@mpl.dev>
2024-11-01 10:24:29 +0000

    Apply suggestions from code review

--- src/content/blogPosts/live-stream-to-the-metaverse/index.ts
+++ src/content/blogPosts/live-stream-to-the-metaverse/index.ts
@@ -5,7 +5,7 @@ import body from "./body.mdx";
 const introducingMml: BlogPost = {
   title: "Live Streaming to the Metaverse",
   description: "An easy way to have your live event in the Metaverse.",
-  date: "2024-10-31",
+  date: "2024-11-01",
   body,
   image: "/images/blog/live-stream-to-the-metaverse.jpg",
 };
