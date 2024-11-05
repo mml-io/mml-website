d6dccaf1daccc1c77257e7ee37bd35b8f2020b7f
TheCodeTherapy <me@mgz.me>
2024-11-05 17:02:44 +0000

    starts drafting the ai-powered-npc blog post

---
+++ src/content/blogPosts/ai-powered-npc/index.ts
@@ -0,0 +1,13 @@
+import { BlogPost } from "@/types/blog-post";
+
+import body from "./body.mdx";
+
+const introducingMml: BlogPost = {
+  title: "AI-Powered NPC with MML",
+  description: "How to create an MML OpenAI Agent in your virtual world.",
+  date: "2024-11-05",
+  body,
+  image: "/images/blog/ai-powered-npc.jpg",
+};
+
+export default introducingMml;
