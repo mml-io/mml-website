93f6bb9b52ad539c35a8ba0088a9e4e482ea4181
TheCodeTherapy <me@mgz.me>
2024-11-12 17:08:28 +0000

    updates blog post with links to anchors

--- src/content/blogPosts/ai-powered-npc/index.ts
+++ src/content/blogPosts/ai-powered-npc/index.ts
@@ -8,6 +8,23 @@ const introducingMml: BlogPost = {
   date: "2024-11-05",
   body,
   image: "/images/blog/ai-powered-npc.jpg",
+  linkList: [
+    "Create an OpenAI Assistant",
+    "Configure your Assistant",
+    "Creating your NPC MML Document",
+    "The requests queue",
+    "Submitting Messages to the Queue",
+    "Processing the Queue",
+    "Sending Messages to OpenAI",
+    "Handling the AI Response",
+    "Text-to-Speech: Giving Voice to your NPC",
+    "Handling Timeout with Speech Generation",
+    "Playing the Audio",
+    "Processing the Audio Response Queue",
+    "Animating and Bringing the NPC to Life",
+    "Proximity Detection and State Reset",
+    "Listening for Chat Messages",
+  ],
 };
 
 export default introducingMml;
